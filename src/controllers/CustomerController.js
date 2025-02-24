da3b7c203a39fb87ac6d6fbccd2055f822262eb1
Owl <practice.tech2022@gmail.com>
2025-02-24 10:58:45 +0800

    API_URL prod and local setup

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+//Hard coded for prod test only
 const API_URL = "http://35.226.41.172/api/customers";
+
+//For local test
+//const API_URL = "http://127.0.0.1:8000/api/customers";
 
 const CustomerController = {
   fetchCustomers: async () => {
